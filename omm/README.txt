bf898603bf132eb528fae9951a4eb305fe87eebb
eliott.tixier <eltix@users.noreply.github.com>
2017-05-22 17:07:24 +0200

    Observable moment matching algorithm with a demo
    Former-commit-id: c45726caea5f37477368c10037cc1b6952bd749e

---
+++ omm/README.txt
@@ -0,0 +1,9 @@
+This the numerical code used to compute the Probability Density Function (PDF)
+from the variability observed in measurements (whether synthetic or
+experimental).
+
+The code is written in C++ (C++98 standard) and is divided into three files:
+main.cpp
+DE.cpp
+DE.hpp
+
---
+++ omm/main.cpp
@@ -0,0 +1,49 @@
+/*=============================================================================
+ This file is part of the code DEMMA
+ Density Estimation using the Moment Matching Approach
+ Copyright (C) 2014, INRIA
+
+ DEMMA is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+
+ DEMMA is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with DEMMA. If not, see http://www.gnu.org/licenses/.
+ =============================================================================*/
+
+/*!
+ \file main.cpp
+ \authors J.-F. Gerbeau, D. Lombardi, E. Tixier
+ \date 05/04/2017
+ \brief
+ */
+
+#include "DE.hpp"
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+
+//  double ti = clock();
+
+  if (argc != 2){puts("Error : Wrong number of arguments. Specify your case name."); return 1;}
+  const string caseName = argv[1];  
+
+  cout << "Estimate the probability density" << endl;
+  const string dirOfLog = "../../../Data/" + caseName;
+  
+  DE estimation;
+  estimation.initialize(dirOfLog);
+  estimation.readData();
+  estimation.findSaddlePoint();
+  estimation.saveResult2();
+  estimation.computeStatistics();
+
+  return 0;
+}
